b5d6cb0ab1f78cddd0375985a7cd082c153c4d4e
Benedikt Berger <benedikt.berger@live.de>
2016-12-16 21:40:02 +0100

    removed cropping

--- src/ng2-img-max.module.ts
+++ src/ng2-img-max.module.ts
@@ -2,7 +2,6 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Ng2ImgMaxService } from './ng2-img-max.service';
 import { ImgMaxSizeService } from './img-max-size.service';
 import { ImgMaxPXSizeService } from './img-maxpx-size.service';
-import { ImgCropService } from './img-crop.service';
 import { Ng2PicaModule } from 'ng2-pica';
 
 @NgModule({
@@ -10,7 +9,6 @@ import { Ng2PicaModule } from 'ng2-pica';
         Ng2PicaModule
     ],
     providers: [
-        {provide: ImgCropService, useClass: ImgCropService},
         {provide: ImgMaxPXSizeService, useClass: ImgMaxPXSizeService},
         {provide: ImgMaxSizeService, useClass: ImgMaxSizeService},
         {provide: Ng2ImgMaxService, useClass: Ng2ImgMaxService}
