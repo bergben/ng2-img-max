36b1bdc081bafc09ade3c4ef83e9459165f54b70
Benedikt Berger <benedikt.berger@live.de>
2016-12-16 21:39:56 +0100

    removed cropping

--- src/ng2-img-max.service.ts
+++ src/ng2-img-max.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
-import { ImgCropService } from './img-crop.service';
 import { ImgMaxSizeService } from './img-max-size.service';
 import { ImgMaxPXSizeService } from './img-maxpx-size.service';
 
 @Injectable()
 export class Ng2ImgMaxService {
-    constructor(private imgMaxSizeService: ImgMaxSizeService, private imgMaxPXSizeService: ImgMaxPXSizeService, private imgCropService: ImgCropService) {
+    constructor(private imgMaxSizeService: ImgMaxSizeService, private imgMaxPXSizeService: ImgMaxPXSizeService) {
     }
     public compress(files: File[], maxSizeInMB: number, logExecutionTime: Boolean = false): Observable<any> {
         let compressedFileSubject: Subject<any> = new Subject<any>();
@@ -25,18 +24,6 @@ export class Ng2ImgMaxService {
             });
         });
         return resizedFileSubject.asObservable();
-    }
-    public crop(files: File[], toWidth: number, toHeight: number, startX: number = 0, startY: number = 0): Observable<any> {
-        let croppedFileSubject: Subject<any> = new Subject<any>();
-        files.forEach((file) => {
-            this.cropImage(file, toWidth, toHeight, startX, startY).subscribe((value) => {
-                croppedFileSubject.next(value);
-            });
-        });
-        return croppedFileSubject.asObservable();
-    }
-    public cropImage(file: File, toWidth: number, toHeight: number, startX: number = 0, startY: number = 0): Observable<any> {
-        return this.imgCropService.cropImage(file, toWidth, toHeight, startX, startY);
     }
     public compressImage(file: File, maxSizeInMB: number, logExecutionTime: Boolean = false): Observable<any> {
         return this.imgMaxSizeService.compressImage(file, maxSizeInMB, logExecutionTime);
