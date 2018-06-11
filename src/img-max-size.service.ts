6d393890dca95cf0d729a05f6565720357c28775
Juan David Maldonado <maldo.s@hotmail.com>
2018-06-11 16:21:05 -0500

    Update to angular 6.0.4, Fix Observable injection

--- src/img-max-size.service.ts
+++ src/img-max-size.service.ts
@@ -1,7 +1,5 @@
 import { Injectable, Inject, forwardRef } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-
+import { Observable, Subject } from 'rxjs';
 
 import { ImgExifService } from './img-exif.service';
 
